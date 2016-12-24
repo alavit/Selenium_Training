f5659d5abd029b26cd5509e95633eb83ecd58bbb
Alexey Avitsuk <alexey.avitsuk@gmail.com>
2016-12-24 20:16:40 +0300

    Admin Login Test

--- java-example/src/test/java/ru/stqa/training/selenium/AdminLogin.java
+++ java-example/src/test/java/ru/stqa/training/selenium/AdminLogin.java
@@ -3,11 +3,14 @@ package ru.stqa.training.selenium;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class MyFirstTest {
+import java.util.concurrent.TimeUnit;
+
+public class AdminLogin {
 
     private WebDriver driver;
     private WebDriverWait wait;
@@ -19,8 +22,11 @@ public class MyFirstTest {
     }
 
     @Test
-    public void myFirstTest() {
-       driver.get("http://www.google.com/");
+    public void adminLogin() {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
     }
 
     @After
